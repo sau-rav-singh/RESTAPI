e6cb36857902484641e2b96f34a553fc3d49981a
Saurav Singh <singh.saurav@icloud.com>
2024-08-04 15:41:50 +0530

    Added RSV Files

--- src/test/java/BasicTest.java
+++ src/test/java/BasicTest.java
@@ -11,32 +11,17 @@ public class BasicTest {
 
         RestAssured.baseURI = "https://rahulshettyacademy.com";
 
-        String postResponse = given().queryParam("key", "qaclick123")
-                .header("Content-Type", "application/json")
-                .body(new File("src/test/resources/AddPlace.json"))
-                .when().post("maps/api/place/add/json")
-                .then()
-                .assertThat().statusCode(200)
-                .body("scope", equalTo("APP")).header("server", "Apache/2.4.52 (Ubuntu)")
-                .extract().response().asString();
+        String postResponse = given().queryParam("key", "qaclick123").header("Content-Type", "application/json").body(new File("src/test/resources/AddPlace.json")).when().post("maps/api/place/add/json").then().assertThat().statusCode(200).body("scope", equalTo("APP")).header("server", "Apache/2.4.52 (Ubuntu)").extract().response().asString();
 
         JsonPath js = new JsonPath(postResponse);
         String place_id = js.get("place_id");
         System.out.println("place_id is " + place_id);
 
-        String putResponse = given().queryParam("place_id", place_id).queryParam("key", "qaclick123").header("Content-Type", "application/json")
-                .body("{\n" +
-                        "\"place_id\":\"" + place_id + "\",\n" +
-                        "\"address\":\"70 Summer walk, IND\",\n" +
-                        "\"key\":\"qaclick123\"\n" +
-                        "}").when().put("maps/api/place/update/json").then().assertThat().statusCode(200)
-                .extract().response()
-                .asString();
+        String putResponse = given().queryParam("place_id", place_id).queryParam("key", "qaclick123").header("Content-Type", "application/json").body("{\n" + "\"place_id\":\"" + place_id + "\",\n" + "\"address\":\"70 Summer walk, IND\",\n" + "\"key\":\"qaclick123\"\n" + "}").when().put("maps/api/place/update/json").then().assertThat().statusCode(200).body("msg",equalTo("Address successfully updated")).extract().response().asString();
 
         System.out.println("putResponse is " + putResponse);
 
-        String getResponse = given().queryParam("key", "qaclick123").queryParam("place_id", place_id).when().get("maps/api/place/get/json")
-                .then().statusCode(200).extract().response().asString();
+        String getResponse = given().queryParam("key", "qaclick123").queryParam("place_id", place_id).when().get("maps/api/place/get/json").then().statusCode(200).extract().response().asString();
 
         System.out.println(getResponse);
 
